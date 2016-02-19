4b25c1e3121a714b7c9c7810e115f43c1d957d8b
Seregfaroth <bl@tokni.com>
2016-02-19 13:05:30 +0000

    temp commit

---
+++ Mareframe-BBN/ts/ElementOO.ts
@@ -0,0 +1,35 @@
+﻿
+module Mareframe {
+    export module DST {
+        export interface ElementOO {
+            m_id: string;
+            m_name: string;
+            m_description: string;
+            m_userDescription: string;
+            m_updated: boolean;
+            m_easelElmt: createjs.Container;
+        }
+
+        export interface ElementOOMca extends ElementOO {
+
+        }
+        export interface ElementOOBbn extends ElementOO {
+            m_minitableEaselElmt: createjs.Container;
+        }
+        export interface ElementOOMcaAttribute extends ElementOOMca {
+
+        }
+        export interface ElementOOMcaObjective extends ElementOOMca {
+
+        }
+        export interface ElementOOMcaAlternative extends ElementOOMca {
+
+        }
+        export interface ElementOOMcaGoal extends ElementOOMca {
+
+        }
+
+    }
+
+    
+}
